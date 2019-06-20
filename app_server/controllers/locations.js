350638baf8337bae50319f8782530aed0728b867
Evan Ogas <evanogas@gmail.com>
2019-06-19 23:35:18 -0400

    move location list data to controller

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,10 +1,49 @@
 
 const homelist = (req, res) => {
-    res.render('locations-list', { title: 'Home' });
+    res.render('locations-list', {
+        title: 'Loc8r - find a place to work with wifi',
+        sidebar: 'Looking for wifi and a seat? Loc8r helps you find places to work when out and about.',
+        pageHeader: {
+            title: 'Loc8r',
+            strapline: 'Find places to work with wifi near you!'
+        },
+        locations: [{
+            name: 'Starcups',
+            address: '125 High Street, Reading RG6 1PS',
+            rating: 3,
+            facilities:[
+                'Hot Drinks',
+                'Food',
+                'Premium wifi'
+            ],
+            distance: '100m'
+        }, {
+            name: 'Cafe Hero',
+            address: '125 High Street, Reading RG6 1PS',
+            rating: 4,
+            facilities:[
+                'Hot Drinks',
+                'Food',
+                'Premium wifi'
+            ],
+            distance: '200m'
+        }, {
+            name: 'Burger Queen',
+            address: '125 High Street, Reading RG6 1PS',
+            rating: 2,
+            facilities:[
+                'Food',
+                'Premium wifi'
+            ],
+            distance: '250m'
+        }]
+    });
 };
 
 const locationInfo = (req, res) => {
-    res.render('location-info', { title: 'Location info' });
+    res.render('location-info', {
+        title: 'Location info'
+    });
 };
 
 const addReview = (req, res) => {
